e3a62854314cffad7a0f9316e718623a3417b5ae
Eymen-Ali <autocadman81@gmail.com>
2025-10-15 23:27:16 +0300

    Add files via upload

---
+++ Robot takimi dersleri/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+    int sayi1=150;
+    int sayi2=5;
+    double bolme= sayi1/sayi2;
+    int toplama= sayi1+sayi2;
+    int carpma= sayi1*sayi2;
+    int cikarma= sayi1-sayi2;
+    System.out.println(toplama);
+    System.out.println(carpma);
+    System.out.println(cikarma);
+    System.out.println(bolme);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}}
---
+++ Robot takimi dersleri/src/ucgen.java
@@ -0,0 +1,13 @@
+public class ucgen {
+
+    public static void main(String[] args) {
+        double eskenarucgeninkenari= 15;
+        double eskenarucgenincevresi = eskenarucgeninkenari*3;
+        double eskenarucgeninyuksekligi = eskenarucgeninkenari/2*Math.sqrt(3);
+        double alan = eskenarucgeninyuksekligi*eskenarucgeninkenari/2;
+        System.out.println("çevre  " + eskenarucgenincevresi);
+        System.out.println("alan  " + alan);
+
+
+
+    }}
