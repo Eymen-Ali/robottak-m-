aff52c10d013a2442e4acf5cbed9b6d731243ce9
Eymen-Ali <autocadman81@gmail.com>
2025-10-29 21:29:00 +0300

    PROBLEM OLMUL TEKRAR YÜKLEDİM
    PROBLEM OLMUL TEKRAR YÜKLEDİM

---
+++ elseifodev4.java
@@ -0,0 +1,23 @@
+public class elseifodev4 {
+
+
+    public static void main(String[] args){
+
+        double vizeler = 30 * 0.6;
+        double finAl = 60 * 0.4;
+        if (vizeler + finAl > 75)
+        System.out.println("Geçtiniz");
+        else System.out.println("Geçemediniz");
+
+
+
+
+
+
+
+
+
+
+
+
+    }}
---
+++ odev3nci.java
@@ -0,0 +1,14 @@
+public class odev3nci {
+    public static void main(String[] args){
+
+
+
+
+
+    String hesapadi = "eymenalieamakine123";
+    System.out.println("Ea makine hesabımıza giriş yapmak için lütfen adınızda küçük harflerle eamakine bulundurum.Eğer ekranda true yazsını görürseniz başarılı demektir. ");
+    System.out.println(hesapadi.contains("eamakine"));
+
+
+
+}}
