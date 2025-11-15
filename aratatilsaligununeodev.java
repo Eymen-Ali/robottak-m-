990023a1a6cc0377876a9f77eacf200664cab7e5
Eymen-Ali <autocadman81@gmail.com>
2025-11-15 19:50:13 +0300

    Update aratatilsaligununeodev.java
    DÜZELTİLDİ

--- aratatilsaligununeodev.java
+++ aratatilsaligununeodev.java
@@ -1,39 +1,27 @@
-import java.util.Arrays;
-
 public class aratatilsaligununeodev {
     public static void main(String[] args){
 
         int[] liste = {37, 4, 82, 59, 11, 96, 73, 28, 65, 17, 53, 88, 92, 41, 6, 30, 79, 2, 45, 51, 23, 13, 57, 67, 48, 99, 9, 71, 60, 85, 19, 31, 44, 33, 50, 26, 8, 97, 12, 70, 81, 39, 61, 64, 25, 3, 90, 56, 68, 15};
-        Arrays.sort(liste);
-        for (int a = 0; a< liste.length;a++){
-            System.out.println(liste[a]);}
+        int[] siralanmis = new int[liste.length];
+        for(int a = 0; a < liste.length; a++){
+            int sinir = 999999;
+            for(int b = 0; b < liste.length; b++){
+                if (liste[b] < sinir){
+                    sinir = liste[b];
+                }
+            }
+            siralanmis[a] = sinir;
+            for(int b = 0; b < liste.length; b++){
+                if(liste[b] == sinir){
+                    liste[b] = 999999;
 
+                }
 
-//Merhaba Semih abi ödevi önce kendi bilgimle yapmaya çalıştım yapamadım. Araştırdım sonradan "Arrays.sort" komutunu buldum.Bu komut dizinin düzenini küçükten büyüğe olacak şekilde ayarlıyormuş.Kullanmak için import java util arrays yazmakta gerekyormuş. Ben arrays sort yazınca otomatik geldi. Silince arrays.sort komutu hata veriyor zaten baktım. Arrays sortdan sonra öğrendiğimiz for komutunu kullandım.
-//Semih abi birde ben 3. ve 4. ödevi zamanında yapmıştım normalde. Githuba doğru yüklediğimi sanmıştım ancak nasıl olduysa içi boş dosyalar yüklemişim sonrdan farkettim. Onları düzelttim bakabilr misin acaba? Hazır ödevime bakmışken yazdığımıda görürsün diye buraya yazmak istedim.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-}
+            }
+        }
+        for(int c : siralanmis) {
+            System.out.println(c);
+        }
 
 
 
