8bc0725b4b81c77fb04b2bc3397a9663117ac502
Eymen-Ali <autocadman81@gmail.com>
2025-11-18 20:12:17 +0300

    OOP ÖDEVİ
    OOP ÖDEVİ

---
+++ dersler.java
@@ -0,0 +1,14 @@
+public class dersler {
+
+String odev;
+String girenhoca;
+int yazilinotu;
+int sozlunotu;
+
+
+public dersler(String odev, String girenhoca, int yazilinotu, int sozlunotu){
+    this.odev = odev;
+    this.girenhoca=girenhoca;
+    this.yazilinotu=yazilinotu;
+    this.sozlunotu=sozlunotu;
+        };}
