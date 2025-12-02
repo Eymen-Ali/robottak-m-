8dfa04730345d1bdbfdcf20442d8125697275fee
Eymen-Ali <autocadman81@gmail.com>
2025-12-02 20:05:50 +0300

    Add files via upload

---
+++ dersgunleri.java
@@ -0,0 +1,16 @@
+public class dersgunleri {
+
+    String birinciders;
+    String ikinciders;
+    String ucuncuders;
+
+
+public dersgunleri(String birinciders,String ikinciders, String ucuncuders) {
+    this .birinciders = birinciders;
+    this .ikinciders = ikinciders;
+    this .ucuncuders = ucuncuders;
+
+
+
+
+}}
